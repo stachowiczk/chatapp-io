e293b92d7dbfa890623d5e9bfd1eb68f6edc4287
Karol Stachowicz <kstachowicz@icloud.com>
2024-02-15 15:53:38 +0100

    added socket initialization

--- src/app.ts
+++ src/app.ts
@@ -11,27 +11,35 @@ import { Server } from 'socket.io';
 
   app.use(cors());
   app.use(express.json());
-  app.use(
-    session({
-      secret: 'secret',
-      resave: false,
-      saveUninitialized: true,
-    })
-  );
+  //app.use(
+  //session({
+  //secret: 'secret',
+  //resave: false,
+  //saveUninitialized: true,
+  //})
+  //);
 
   const io = new Server(server, {
     cors: {
       origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
     },
   });
 
-  const users: Record<string, string> = {};
+  let connectedUsers: Record<string, string> = {};
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    connectedUsers[socket.id] = 'user';
+    console.log('connectedUsers', connectedUsers);
+    socket.on('message', (text) => {
+      console.log('message', {text});
+      io.emit('message', text);
+    });
+  });
+
   const testMessage = {
     id: 1,
     text: 'Messages test',
   };
-  
 
   app.get('/api/messages', (req, res) => {
     res.json([testMessage]);
@@ -40,7 +48,7 @@ import { Server } from 'socket.io';
   app.post('/api/login', async (req, res) => {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    users[username] = hashedPassword;
+    connectedUsers[username] = hashedPassword;
     res.status(201).send();
   });
 
@@ -50,34 +58,7 @@ import { Server } from 'socket.io';
     });
   });
 
-  app.post('/api/messages', (req, res) => {
-    const { text } = req.body;
-    if (text) {
-      io.emit('message', text);
-      res.status(201).send();
-    } else {
-      res.status(400).send();
-    }
-  });
-
-  app.post('/api/messages', (req, res) => {
-    const { text } = req.body;
-    if (text) {
-      io.emit('message', text);
-      res.status(201).send();
-    } else {
-      res.status(400).send();
-    }
-  });
-
-  io.on('message', (text) => {
-    console.log('message', text);
-  });
-
-
   server.listen(3001, () => {
     console.log('Server is listening on port 3001');
   });
-
-
 })();
--- src/client/client/src/components/Messages.tsx
+++ src/client/client/src/components/Messages.tsx
@@ -1,25 +1,41 @@
 import React, { FormEvent, useState, useEffect } from 'react';
 import axios from 'axios';
+import { io, Socket } from 'socket.io-client';
+import { set } from 'mongoose';
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState('');
+  const [socketId, setSocketId] = useState<string | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/api/messages').then((response) => {
-      setMessages(response.data);
+    const newSocket = io('http://localhost:3001');
+
+    newSocket.on('connect', () => {
+      console.log('connected to socketio');
+      if (newSocket.id) {
+        setSocketId(newSocket.id);
+      }
+      setSocket(newSocket);
     });
+
+    return () => {
+      newSocket.disconnect();
+    };
   }, []);
 
   const sendMessage = (text: string) => {
-    axios.post('http://localhost:3001/api/messages', { text });
-  }
+    if (socket) {
+      socket.emit('message', text);
+    }
+  };
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     sendMessage(text);
     setText('');
-  }
+  };
 
   return (
     <div>
@@ -29,9 +45,14 @@ const Messages = () => {
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
+      <p>Connected to socket.io {socketId}</p>
     </div>
   );
 };
